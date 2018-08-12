9e37fcebd97fdfcc98da6598f52dd33072a10005
Henry Campos <henry.campos@galileo.edu>
2018-08-12 01:27:53 -0600

    Repositorio base de Krasters
    Carpetas y archivos base.

---
+++ build/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Krasters</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/main.min.css" />
+</head>
+
+<body>
+    <section id="header">
+        <div class="wrapper">
+            KRAS<b>TERS</b>
+        </div>
+    </section>
+    <script src="./assets/js/vendors.js"></script>
+    <script src="./assets/js/app.js"></script>
+</body>
+
+</html>
