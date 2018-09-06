87ae22f89c7c0ce7c20189dccbef6540ed43c053
Henry Campos <henry.campos@galileo.edu>
2018-09-06 15:55:10 -0600

    Merged changes added

--- build/assets/php/modules/nav.php
+++ build/assets/php/modules/nav.php
@@ -20,7 +20,6 @@
                     <li><a href="" class="nav-logo"><b>KRAS</b>TERS</a></li>
                 </span>
                 <span class="options-division">
-<<<<<<< HEAD
                     <?php 
                         if($role == 1){
                             echo '<li class="nav-user"><a href=""><b>usuario</b>: user</a></li>';
@@ -45,13 +44,6 @@
                             echo '<li><a href="../session/logout.php" class="nav-option"><span class="option-icon"><i class="fas fa-sign-out-alt"></i></span><span class="option-label">Salir</span></a></li>';
                         }
                     ?>
-=======
-                    <li class="nav-user"><a href=""><b>usuario</b>: user</a></li>
-                    <li><a href="../shop/home.php" class="nav-option"><span class="option-icon"><i class="fas fa-shopping-bag"></i></span><span class="option-label">Tienda</span></a></li>
-                    <!-- En el span .counter se debe colocar el tamaño del detalle. -->
-                    <li><a href="../shop/shopping_cart.php" class="nav-option"><span class="option-icon"><i class='fa fa-shopping-cart'></i></span><span class="option-label">Carrito</span><span class="counter">2</span></a></li>
-                    <li><a href="" class="nav-option"><span class="option-icon"><i class="fas fa-sign-out-alt"></i></span><span class="option-label">Salir</span></a></li>
->>>>>>> 22b71a42e7045e134fb1ffc52a0e64f244b48a31
                 </span>
             </ul>
         </nav>
