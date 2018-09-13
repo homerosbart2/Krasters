a1046a3cc4348336c280c7e020c6a67e8103c4aa
Diego alejandro Alay <diego.alay@galileo.edu>
2018-09-13 00:01:44 -0600

    uploads listos, shopping cart casi listo

--- build/assets/php/rutas_ajax/carrito/listado.php
+++ build/assets/php/rutas_ajax/carrito/listado.php
@@ -2,7 +2,7 @@
 //Lista los productos actuales del cliente
     $link = pg_connect("host=localhost dbname=TIENDA user=tienda password=%TiendaAdmin18%");
     session_start();
-    $usuario = $_SESSION['username']; //variable que se obtiene con la cookie
+    $usuario = 'diego'; //variable que se obtiene con la cookie
     $result_array = array();
     $query = "SELECT * FROM Carrito AS Ca, Colores AS Co, Productos AS P, Marcas AS M where Ca.cantidad > 0 AND Ca.usuario='$usuario' AND Ca.color_nombre = Co.color_nombre AND Ca.producto_id = P.producto_id AND P.marca_nombre = M.marca_nombre ORDER BY P.producto_nombre,Ca.carrito_id";
     $result = pg_query($link, $query); 
